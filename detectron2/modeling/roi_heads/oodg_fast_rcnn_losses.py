4278b2704c0f077040689297dcf76533c37d751e
Daniel Nobbe <daniellnobbe@gmail.com>
2020-12-29 15:14:52 +0100

    Added import of base class for outputs:

--- detectron2/modeling/roi_heads/oodg_fast_rcnn_losses.py
+++ detectron2/modeling/roi_heads/oodg_fast_rcnn_losses.py
@@ -11,6 +11,7 @@ from detectron2.modeling.box_regression import Box2BoxTransform
 from detectron2.structures import Boxes, Instances
 from detectron2.utils.events import get_event_storage
 
+from .fast_rcnn import FastRCNNOutputs
 
 class OodgFastRCNNOutputs(FastRCNNOutputs):
     def __init__(
