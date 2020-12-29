6b01d6117ed4f355d04a03a5d751dbf0c3db30ab
Daniel Nobbe <daniellnobbe@gmail.com>
2020-12-29 15:17:34 +0100

    Added class to __all__

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -15,7 +15,7 @@ from detectron2.utils.events import get_event_storage
 from .oodg_fast_rcnn_losses import OodgFastRCNNOutputs
 
 __all__ = ["fast_rcnn_inference", "FastRCNNOutputLayers", 
-            "OodgFastRCNNOutputLayers"]
+            "OodgFastRCNNOutputLayers", "FastRCNNOutputs"]
 
 
 logger = logging.getLogger(__name__)
