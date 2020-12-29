28fc6de79435cb8ed542738c77d20a56ec9b2673
Daniel Nobbe <daniellnobbe@gmail.com>
2020-12-29 17:37:49 +0100

    debuging

--- detectron2/modeling/proposal_generator/rpn.py
+++ detectron2/modeling/proposal_generator/rpn.py
@@ -423,6 +423,7 @@ class RPN(nn.Module):
         anchors = self.anchor_generator(features)
 
         pred_objectness_logits, pred_anchor_deltas = self.rpn_head(features)
+        # Each of these still has images in batch as the first dim
         # Transpose the Hi*Wi*A dimension to the middle:
         pred_objectness_logits = [
             # (N, A, Hi, Wi) -> (N, Hi, Wi, A) -> (N, Hi*Wi*A)
@@ -515,33 +516,135 @@ class OodgRPN(RPN):
     @classmethod
     def from_config(cls, cfg, input_shape: Dict[str, ShapeSpec]):
         return super(OodgRPN, cls).from_config(cfg, input_shape)
-        # For some reason super from config does not work
-        # Makes sense to not use it tho
-        # Let's just copy it
-        # in_features = cfg.MODEL.RPN.IN_FEATURES
-        # ret = {
-        #     "in_features": in_features,
-        #     "min_box_size": cfg.MODEL.PROPOSAL_GENERATOR.MIN_SIZE,
-        #     "nms_thresh": cfg.MODEL.RPN.NMS_THRESH,
-        #     "batch_size_per_image": cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE,
-        #     "positive_fraction": cfg.MODEL.RPN.POSITIVE_FRACTION,
-        #     "loss_weight": {
-        #         "loss_rpn_cls": cfg.MODEL.RPN.LOSS_WEIGHT,
-        #         "loss_rpn_loc": cfg.MODEL.RPN.BBOX_REG_LOSS_WEIGHT * cfg.MODEL.RPN.LOSS_WEIGHT,
-        #     },
-        #     "anchor_boundary_thresh": cfg.MODEL.RPN.BOUNDARY_THRESH,
-        #     "box2box_transform": Box2BoxTransform(weights=cfg.MODEL.RPN.BBOX_REG_WEIGHTS),
-        #     "box_reg_loss_type": cfg.MODEL.RPN.BBOX_REG_LOSS_TYPE,
-        #     "smooth_l1_beta": cfg.MODEL.RPN.SMOOTH_L1_BETA,
-        # }
+        # Nice, this works
 
-        # ret["pre_nms_topk"] = (cfg.MODEL.RPN.PRE_NMS_TOPK_TRAIN, cfg.MODEL.RPN.PRE_NMS_TOPK_TEST)
-        # ret["post_nms_topk"] = (cfg.MODEL.RPN.POST_NMS_TOPK_TRAIN, cfg.MODEL.RPN.POST_NMS_TOPK_TEST)
+    def forward(
+        self,
+        images: ImageList,
+        features: Dict[str, torch.Tensor],
+        oodg_dataset_numbers: List, # Of the same length as number of images
+        gt_instances: Optional[List[Instances]] = None,
+    ):
+        """
+        Args:
+            images (ImageList): input images of length `N`
+            features (dict[str, Tensor]): input data as a mapping from feature
+                map name to tensor. Axis 0 represents the number of images `N` in
+                the input data; axes 1-3 are channels, height, and width, which may
+                vary between feature maps (e.g., if a feature pyramid is used).
+            gt_instances (list[Instances], optional): a length `N` list of `Instances`s.
+                Each `Instances` stores ground-truth instances for the corresponding image.
 
-        # ret["anchor_generator"] = build_anchor_generator(cfg, [input_shape[f] for f in in_features])
-        # ret["anchor_matcher"] = Matcher(
-        #     cfg.MODEL.RPN.IOU_THRESHOLDS, cfg.MODEL.RPN.IOU_LABELS, allow_low_quality_matches=True
-        # )
-        # ret["head"] = build_rpn_head(cfg, [input_shape[f] for f in in_features])
-        # return ret
+        Returns:
+            proposals: list[Instances]: contains fields "proposal_boxes", "objectness_logits"
+            loss: dict[Tensor] or None
+        """
+        features = [features[f] for f in self.in_features]
+        anchors = self.anchor_generator(features)
+
+        pred_objectness_logits, pred_anchor_deltas = self.rpn_head(features)
+        # Transpose the Hi*Wi*A dimension to the middle:
+        pred_objectness_logits = [
+            # (N, A, Hi, Wi) -> (N, Hi, Wi, A) -> (N, Hi*Wi*A)
+            score.permute(0, 2, 3, 1).flatten(1)
+            for score in pred_objectness_logits
+        ]
+        pred_anchor_deltas = [
+            # (N, A*B, Hi, Wi) -> (N, A, B, Hi, Wi) -> (N, Hi, Wi, A, B) -> (N, Hi*Wi*A, B)
+            x.view(x.shape[0], -1, self.anchor_generator.box_dim, x.shape[-2], x.shape[-1])
+            .permute(0, 3, 4, 1, 2)
+            .flatten(1, -2)
+            for x in pred_anchor_deltas
+        ]
+
+        if self.training:
+            assert gt_instances is not None, "RPN requires gt_instances in training!"
+            gt_labels, gt_boxes = self.label_and_sample_anchors(anchors, gt_instances)
+            # everything is still per-image here
+            losses = self.losses(
+                anchors, pred_objectness_logits, gt_labels, pred_anchor_deltas, gt_boxes, oodg_dataset_numbers
+            )
+        else:
+            losses = {}
+        proposals = self.predict_proposals(
+            anchors, pred_objectness_logits, pred_anchor_deltas, images.image_sizes
+        )
+        return proposals, losses
         
+    @torch.jit.unused
+    def losses(
+        self,
+        anchors: List[Boxes],
+        pred_objectness_logits: List[torch.Tensor],
+        gt_labels: List[torch.Tensor],
+        pred_anchor_deltas: List[torch.Tensor],
+        gt_boxes: List[torch.Tensor],
+        oodg_dataset_numbers: List,
+    ) -> Dict[str, torch.Tensor]:
+        """
+        Return the losses from a set of RPN predictions and their associated ground-truth.
+
+        Args:
+            anchors (list[Boxes or RotatedBoxes]): anchors for each feature map, each
+                has shape (Hi*Wi*A, B), where B is box dimension (4 or 5).
+            pred_objectness_logits (list[Tensor]): A list of L elements.
+                Element i is a tensor of shape (N, Hi*Wi*A) representing
+                the predicted objectness logits for all anchors.
+            gt_labels (list[Tensor]): Output of :meth:`label_and_sample_anchors`.
+            pred_anchor_deltas (list[Tensor]): A list of L elements. Element i is a tensor of shape
+                (N, Hi*Wi*A, 4 or 5) representing the predicted "deltas" used to transform anchors
+                to proposals.
+            gt_boxes (list[Tensor]): Output of :meth:`label_and_sample_anchors`.
+
+        Returns:
+            dict[loss name -> loss value]: A dict mapping from loss name to loss value.
+                Loss names are: `loss_rpn_cls` for objectness classification and
+                `loss_rpn_loc` for proposal localization.
+        """
+        num_images = len(gt_labels)
+        gt_labels = torch.stack(gt_labels)  # (N, sum(Hi*Wi*Ai))
+
+        # Log the number of positive/negative anchors per-image that's used in training
+        pos_mask = gt_labels == 1
+        num_pos_anchors = pos_mask.sum().item()
+        num_neg_anchors = (gt_labels == 0).sum().item()
+        storage = get_event_storage()
+        storage.put_scalar("rpn/num_pos_anchors", num_pos_anchors / num_images)
+        storage.put_scalar("rpn/num_neg_anchors", num_neg_anchors / num_images)
+
+        # if self.box_reg_loss_type == "smooth_l1":
+        anchors = type(anchors[0]).cat(anchors).tensor  # Ax(4 or 5)
+        gt_anchor_deltas = [self.box2box_transform.get_deltas(anchors, k) for k in gt_boxes]
+        gt_anchor_deltas = torch.stack(gt_anchor_deltas)  # (N, sum(Hi*Wi*Ai), 4 or 5)
+        localization_loss = smooth_l1_loss(
+            cat(pred_anchor_deltas, dim=1)[pos_mask],
+            gt_anchor_deltas[pos_mask],
+            self.smooth_l1_beta,
+            reduction="none",
+        )
+        print("loc. loss shape: ", localization_loss.shape)
+        # elif self.box_reg_loss_type == "giou":
+        #     pred_proposals = self._decode_proposals(anchors, pred_anchor_deltas)
+        #     pred_proposals = cat(pred_proposals, dim=1)
+        #     pred_proposals = pred_proposals.view(-1, pred_proposals.shape[-1])
+        #     pos_mask = pos_mask.view(-1)
+        #     localization_loss = giou_loss(
+        #         pred_proposals[pos_mask], cat(gt_boxes)[pos_mask], reduction="sum"
+        #     )
+        # else:
+        #     raise ValueError(f"Invalid rpn box reg loss type '{self.box_reg_loss_type}'")
+
+        valid_mask = gt_labels >= 0
+        objectness_loss = F.binary_cross_entropy_with_logits(
+            cat(pred_objectness_logits, dim=1)[valid_mask],
+            gt_labels[valid_mask].to(torch.float32),
+            reduction="none",
+        )
+        print("obj. loss shape: ", objectness_loss.shape)
+        normalizer = self.batch_size_per_image * num_images
+        losses = {
+            "loss_rpn_cls": objectness_loss / normalizer,
+            "loss_rpn_loc": localization_loss / normalizer,
+        }
+        losses = {k: v * self.loss_weight.get(k, 1.0) for k, v in losses.items()}
+        return losses
