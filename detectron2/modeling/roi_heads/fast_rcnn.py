26ac65d7e71e094a8b80dc9920c9cd6b282cf723
Daniel Nobbe <daniellnobbe@gmail.com>
2020-12-30 14:45:33 +0100

    Modifed line

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -435,7 +435,7 @@ class OodgFastRCNNOutputs(FastRCNNOutputs):
         # with the oodg dataset number of the image it belongs to
         oodg_bbox_mask = self.prop_dataset_numbers.view(-1,1).to(device) * fg_inds
         bbox_dataset_numbers = oodg_bbox_mask[fg_inds]
-
+        set_trace()
         if cls_agnostic_bbox_reg:
             # pred_proposal_deltas only corresponds to foreground class for agnostic
             gt_class_cols = torch.arange(box_dim, device=device)
