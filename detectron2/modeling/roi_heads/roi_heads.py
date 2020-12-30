14fbb5efe3eded33779bebbcceb749928f332af5
Daniel Nobbe <daniellnobbe@gmail.com>
2020-12-30 14:14:45 +0100

    Added set_trace:

--- detectron2/modeling/roi_heads/roi_heads.py
+++ detectron2/modeling/roi_heads/roi_heads.py
@@ -1072,6 +1072,8 @@ class OodgROIHeads(StandardROIHeads):
         # D: Predictions is a tuple of scores, bbox_deltas, where scores is a tensor of (M, K+1) (K object classes, 1 background class)
         # D: bbox_deltas is a tensor of (M, 4K)
         del box_features
+        # print("Proposal shape":)
+        set_trace()
         if self.training:
             losses = self.box_predictor.losses(predictions, proposals, prop_dataset_numbers)
             # proposals is modified in-place below, so losses must be computed first.
